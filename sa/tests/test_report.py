9a94053cfdf645ce52e13202dda48319063d9ba3
Abdelrahaman Abdelkader <darkel1911@gmail.com>
2026-06-10 10:27:30 +0300

    Delete sa directory

--- sa/tests/test_report.py
+++
@@ -1,9 +0,0 @@
-from sa.nodes.report import calculate_grade
-
-def test_calculate_grade():
-    assert calculate_grade(95) == "A"
-    assert calculate_grade(90) == "A"
-    assert calculate_grade(89.9) == "B"
-    assert calculate_grade(75) == "C"
-    assert calculate_grade(60) == "D"
-    assert calculate_grade(59) == "F"
